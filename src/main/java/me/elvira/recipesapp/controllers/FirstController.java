0e31605b7fdd5209f44f2b608e84eaf61cd7970c
Elvira <rostechno_rb@mail.ru>
2023-03-12 21:55:00 +0500

    homework 2 course 3 completed

---
+++ src/main/java/me/elvira/recipesapp/controllers/FirstController.java
@@ -0,0 +1,29 @@
+package me.elvira.recipesapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+public class FirstController {
+    @GetMapping("/")
+    public String helloWord() {
+        return "Hello, web";
+    }
+
+    @RestController
+    public class Page {
+        @GetMapping("/info")
+        public String Info() {
+            String name = "Колобок";
+            String project = "Побег";
+            LocalDate data = LocalDate.of(2023,3,12);
+            String infoProject = "я от бабушки убежал";
+            return "имя ученика: " + name +
+                    ", название проекта: " + project +
+                    ", дата создания проекта: " + data +
+                    ", описание проекта: " + infoProject;
+        }
+    }
+}
